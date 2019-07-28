e1029d1eacd31de3dd88a09c0e2dfcf2911e06e9
Cristeen Olivero <caolivero@liberty.edu>
2019-07-27 23:08:36 -0400

    7_27

---
+++ app/src/main/java/com/example/disinfection_tracking/DisinfectionScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.disinfection_tracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisinfectionScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disinfection_scan);
+    }
+}
--- app/src/main/java/com/example/disinfection_tracking/TransducerActivity.java
+++ app/src/main/java/com/example/disinfection_tracking/TransducerActivity.java
@@ -2,13 +2,23 @@ package com.example.disinfection_tracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TransducerActivity extends AppCompatActivity {
+
+    public void clickSave (View view){
+
+    Intent Intent = new Intent(TransducerActivity.this, DisinfectionScanActivity.class);
+    startActivity(Intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transducer);
+
+
     }
 }
