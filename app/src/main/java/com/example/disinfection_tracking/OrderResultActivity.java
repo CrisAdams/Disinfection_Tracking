8a7f60111de8fce221d40f38d4b64b49cd23d9a0
Cristeen Olivero <caolivero@liberty.edu>
2019-08-15 14:48:03 -0400

    Capstone_Phase3

--- app/src/main/java/com/example/disinfection_tracking/OrderResultActivity.java
+++ app/src/main/java/com/example/disinfection_tracking/OrderResultActivity.java
@@ -1,32 +1,22 @@
 package com.example.disinfection_tracking;
 
-//lines 5-141 written by Cristeen Adams
+//lines 5-131 written by Cristeen Adams
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-
-
 import android.widget.TextView;
-
-
-
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class OrderResultActivity extends AppCompatActivity {
 
